e0b44f7ce75d1b5a8f8fceab892004b9d3a4de02
Anders Vestergaard <mufas.anders@hotmail.com>
2017-11-09 11:11:18 +0100

    Sådan

---
+++ Client/Client/ClientDemo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class ClientDemo
+    {
+        private TcpClient _client;
+        private StreamReader _sReader;
+        private StreamWriter _sWriter;
+        private bool _isConnected;
+
+        public ClientDemo(string ipAdress, int portNum)
+        {
+            _client = new TcpClient();
+            _client.Connect(ipAdress, portNum);
+            HandleCommunication();
+        }
+
+        public void HandleCommunication()
+        {
+            _sReader = new StreamReader(_client.GetStream(), Encoding.ASCII);
+            _sWriter = new StreamWriter(_client.GetStream(), Encoding.ASCII);
+
+            _isConnected = true;
+            string sData = null;
+            string sDataIncoming = string.Empty;
+
+            while (_isConnected)
+            {
+                Console.WriteLine("I got contact with the server.");
+                sData = Console.ReadLine();
+                _sWriter.WriteLine(sData);
+                try
+                {
+                    _sWriter.Flush(); //Ensures that all data is sendt
+                    sDataIncoming = _sReader.ReadLine();
+                }
+                catch
+                {
+                    Console.WriteLine("The server is gone.");
+                    Console.ReadLine();
+                    Thread.CurrentThread.Abort();
+                }
+                if (sDataIncoming != string.Empty)
+                {
+                    Console.WriteLine(sDataIncoming);
+                }
+            }
+        }
+    }
+}
--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Net.Sockets;
 
 namespace Client
 {
@@ -10,6 +13,7 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            ClientDemo client = new ClientDemo("127.0.0.1", 5555);
         }
     }
 }
